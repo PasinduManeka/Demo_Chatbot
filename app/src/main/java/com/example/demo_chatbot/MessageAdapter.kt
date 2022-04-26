5f1fec48cb5abf341a9dfb632e72965fcbf3389e
manekaherat815@gmail.com <manekaherat815@gmail.com>
2022-04-26 09:16:48 +0530

    CREATED layout file for message list

--- app/src/main/java/com/example/demo_chatbot/MessageAdapter.kt
+++ app/src/main/java/com/example/demo_chatbot/MessageAdapter.kt
@@ -1,4 +1,30 @@
 package com.example.demo_chatbot
 
-class MessageAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MessageAdapter (var context:Context,var messageList:ArrayList<MessageClass>):RecyclerView.Adapter<MessageAdapter.MessageViewHolder>(){
+    class MessageViewHolder(view:View):RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MessageAdapter.MessageViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.message_list_item,parent,false)
+        return MessageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MessageAdapter.MessageViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
